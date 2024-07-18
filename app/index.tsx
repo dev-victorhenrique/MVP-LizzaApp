41d17aed0ddde465553ea64f7492af1fc1ff7c6f
Murilo1501 <127565206+Murilo1501@users.noreply.github.com>
2024-07-18 16:55:44 -0300

    introducing gyroscope sensor

--- app/index.tsx
+++ app/index.tsx
@@ -1,42 +1,76 @@
 import { Text, View } from "react-native";
-import { DeviceMotion, DeviceMotionMeasurement,  } from "expo-sensors";
+import {Accelerometer,Gyroscope,AccelerometerMeasurement, GyroscopeMeasurement} from 'expo-sensors'
 import { useState, useEffect, useRef } from "react";
 import { Modalize } from "react-native-modalize";
 import { Subscription } from "expo-sensors/build/Pedometer";
 
-export default function detectFall () {
-    const modalTest = useRef<Modalize>(null)
-    
-    const [data, setData] = useState({
-        acceleration: { x: 0, y: 0, z: 0 }
-    })
 
-    const dataCalculation = () => {
+export default function detectFall(){
+    const modalTest = useRef<Modalize>(null)
+
+    const GRAVITY_ACELERATION = 9.8
+
+   const [data,setData] = useState({
+    acceleration:{x:0,y:0,z:0},
+    rotation : {x:0,y:0,z:0}
+   })
+
+   const magnitudeVectorAcelerometer = () =>{
         let acceleration_x: number = data.acceleration.x
         let acceleration_y: number = data.acceleration.y
         let acceleration_z: number = data.acceleration.z
 
         return Math.sqrt(Math.pow(acceleration_x, 2) + Math.pow(acceleration_y, 2) + Math.pow(acceleration_z, 2))
-    }
+   }
 
-    useEffect(() => {
-        DeviceMotion.setUpdateInterval(500);
-        
-        const subscription: Subscription = DeviceMotion.addListener((motionData: DeviceMotionMeasurement) => {
-            setData((prevData) => ({
-                ...prevData,
-                acceleration: motionData.acceleration ?? prevData.acceleration
-            }));
-        });
-    }, []);
+   const magnitudeVectorGyroscope = () =>{
+        let rotation_x: number = data.rotation.x
+        let rotation_y: number = data.rotation.y
+        let rotation_z: number = data.rotation.z
 
-    console.log(`Aceleração resultante: ${dataCalculation().toFixed(2)}`)
+        return Math.sqrt(Math.pow(rotation_x, 2) + Math.pow(rotation_y, 2) + Math.pow(rotation_z, 2))
+   }
 
-    if (dataCalculation() > DeviceMotion.Gravity) {
+   useEffect(() => {
+    Accelerometer.setUpdateInterval(500);
+    Gyroscope.setUpdateInterval(500)
+    
+    const accelerometerSubscription: Subscription = Accelerometer.addListener((motionData: AccelerometerMeasurement) => {
+        setData((prevData) => ({
+            ...prevData,
+            acceleration: {
+                x: motionData.x,
+                y: motionData.y,
+                z: motionData.z
+            }
+        }));
+    });
+
+    const gyroscopeSubscription: Subscription = Gyroscope.addListener((motionData: GyroscopeMeasurement) => {
+        setData((prevData) => ({
+            ...prevData,
+            rotation: {
+                x: motionData.x,
+                y: motionData.y,
+                z: motionData.z
+            }
+        }));
+    });
+
+
+}, []);
+
+//console.log(`Aceleração resultante: ${magnitudeVectorAcelerometer().toFixed(2)}`)
+
+    if(magnitudeVectorAcelerometer() > GRAVITY_ACELERATION && magnitudeVectorGyroscope() > 1){
         modalTest.current?.open()
     }
+
     
-    return(
+
+
+
+    return (
         <View style={{
             flex: 1,
             justifyContent: 'center',
@@ -48,7 +82,7 @@ export default function detectFall () {
             </Text>
 
             <Text>
-                Aceleração resultante: {dataCalculation().toFixed(2)} / Queda detectada: {dataCalculation() - DeviceMotion.Gravity > 0 ? 'Sim' : 'Não'}
+                Aceleração resultante: {magnitudeVectorAcelerometer().toFixed(2)} / Queda detectada: {magnitudeVectorAcelerometer() - 9.8 > 0 ? 'Sim' : 'Não'}
             </Text>
         
             <Modalize 
@@ -63,4 +97,4 @@ export default function detectFall () {
             </Modalize>
         </View>
     )
-}
+}   
